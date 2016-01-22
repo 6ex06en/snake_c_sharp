ee764d08ceec5ec87514104a49193c817b78d5f1
6ex06en <6ex06en@gmail.com>
2016-01-22 12:15:23 +0300

    Добавлен родительский класс Figure для классов HorizontalLine и VerticalLine

--- Snake/Snake/HorizontalLine.cs
+++ Snake/Snake/HorizontalLine.cs
@@ -6,22 +6,13 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class HorizontalLine
+    class HorizontalLine : Figure
     {
-        List<Point> points = new List<Point>();
-
         public HorizontalLine(int _leftX, int _rightX, int _y, char _sym)
         {
             for(int x = _leftX; x <= _rightX; x++)
             {
                 points.Add(new Point(x, _y, _sym));
-            }
-        }
-        public void Drow()
-        {
-            foreach( Point p in points)
-            {
-                p.Drow();
             }
         }
     }
--- Snake/Snake/VerticalLine.cs
+++ Snake/Snake/VerticalLine.cs
@@ -6,22 +6,13 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class VerticalLine
+    class VerticalLine : Figure
     {
-        List<Point> points = new List<Point>();
-
         public VerticalLine(int _bottomY, int _topY, int _x, char _sym)
         {
             for(int y = _bottomY; y >= _topY; y--)
             {
                 points.Add(new Point(_x, y, _sym));
-            }
-        }
-        public void Drow()
-        {
-            foreach(Point p in points)
-            {
-                p.Drow();
             }
         }
     }
